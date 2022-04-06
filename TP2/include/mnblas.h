a9904f8ebea8c7fef99114ae377312b33b546114
Lucas <lucas.mathieu1@etu.univ-grenoble-alpes.fr>
2022-04-06 17:50:35 +0200

    Merge

--- TP2/include/mnblas.h
+++ TP2/include/mnblas.h
@@ -159,8 +159,6 @@ double mnblas_dznrm2(const int N, const void *X, const int incX);
  * ===========================================================================
  */
 
-/*
-
 void mncblas_sgemv(const MNCBLAS_LAYOUT layout,
                  const MNCBLAS_TRANSPOSE TransA, const int M, const int N,
                  const float alpha, const float *A, const int lda,
@@ -185,15 +183,11 @@ void mncblas_zgemv(MNCBLAS_LAYOUT layout,
                  const void *X, const int incX, const void *beta,
                  void *Y, const int incY);
 
-*/
-
 /*
  * ===========================================================================
  * Prototypes for level 3 BLAS
  * ===========================================================================
  */
-
-/*
 
 void mncblas_sgemm(MNCBLAS_LAYOUT layout, MNCBLAS_TRANSPOSE TransA,
                  MNCBLAS_TRANSPOSE TransB, const int M, const int N,
@@ -201,7 +195,7 @@ void mncblas_sgemm(MNCBLAS_LAYOUT layout, MNCBLAS_TRANSPOSE TransA,
                  const int lda, const float *B, const int ldb,
                  const float beta, float *C, const int ldc);
 
-void mncblas_dgemm(MNCBLAS_LAYOUT layout, CBLAS_TRANSPOSE TransA,
+void mncblas_dgemm(MNCBLAS_LAYOUT layout, MNCBLAS_TRANSPOSE TransA,
                  MNCBLAS_TRANSPOSE TransB, const int M, const int N,
                  const int K, const double alpha, const double *A,
                  const int lda, const double *B, const int ldb,
@@ -218,6 +212,4 @@ void mncblas_zgemm(MNCBLAS_LAYOUT layout, MNCBLAS_TRANSPOSE TransA,
                  const int K, const void *alpha, const void *A,
                  const int lda, const void *B, const int ldb,
                  const void *beta, void *C, const int ldc);
-
-*/
 
