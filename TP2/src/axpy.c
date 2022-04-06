950fde54fb4d77d069941dd4a66ebbe7e592e270
Eloi Charra <eloi.charra@etu.univ-grenoble-alpes.fr>
2022-04-06 17:13:08 +0200

    OpenMP for BLAS1

--- TP2/src/axpy.c
+++ TP2/src/axpy.c
@@ -8,7 +8,8 @@ void mncblas_saxpy(const int N, const float a, const float *X, const int incX,
 {
   register unsigned int i = 0 ;
   register unsigned int j = 0 ;
-  
+
+    #pragma omp parallel for
   for (i = 0 ; i < N ; i += incX)
     {
       Y[j] += a * X [i] * Y [j] ;
@@ -22,7 +23,8 @@ void mncblas_daxpy(const int N, const double a, const double *X, const int incX,
 {
     register unsigned int i = 0 ;
     register unsigned int j = 0 ;
-  
+
+#pragma omp parallel for
     for (i = 0 ; i < N ; i += incX){
         Y[j] += a * X [i] * Y [j] ;
         j+=incY ;
@@ -34,7 +36,8 @@ void   mncblas_caxpy(const int N, const void *a, const void *X, const int incX,
 {
     register unsigned int i = 0 ;
     register unsigned int j = 0 ;
-  
+
+#pragma omp parallel for
     for (i = 0 ; i < N ; i += incX){
         //complexe_float_t multiplication = mult_complexe_float(*(complexe_float_t *)a, ((complexe_float_t *)X)[i]);
         ((complexe_float_t *)Y)[j] = add_complexe_float(mult_complexe_float(*(complexe_float_t *)a, ((complexe_float_t *)X)[i]), ((complexe_float_t *)Y)[j]);
@@ -48,7 +51,8 @@ void   mncblas_zaxpy(const int N, const void *a, const void *X, const int incX,
 {
     register unsigned int i = 0 ;
     register unsigned int j = 0 ;
-  
+
+#pragma omp parallel for
     for (i = 0 ; i < N ; i += incX){
         complexe_double_t multiplication = mult_complexe_double(*(complexe_double_t *)a, ((complexe_double_t *)X)[i]);
         ((complexe_double_t *)Y)[j] = add_complexe_double(multiplication, ((complexe_double_t *)Y)[j]);
